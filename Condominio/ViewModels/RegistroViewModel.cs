d959042811b84b254699b4ac36b8752485984ac9
RobsonSantos <robson@outlook.com>
2022-07-08 09:53:44 -0300

    Aula 9

---
+++ Condominio/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Condominio.ViewModels
+{
+    public class RegistroViewModel
+    {
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Foto { get; set; }
+
+    }
+}

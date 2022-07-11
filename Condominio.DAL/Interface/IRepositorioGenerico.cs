4ef61e4315ed85effdbd81822b947b9fecbe72ed
RobsonSantos <robson@outlook.com>
2022-07-11 15:16:24 -0300

    Aula 10, Interface e Repositorio do Usuário

---
+++ Condominio.DAL/Interface/IRepositorioGenerico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condominio.DAL.Interface
+{
+    public  interface IRepositorioGenerico<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> PegarTodos();
+        Task<TEntity> PegarPeloId(int id);
+        Task<TEntity> PegarPeloId(string id);
+
+        Task Inserir(TEntity entity);
+        Task Atulizar(TEntity entity);
+        Task Excluir(TEntity entity);
+        Task Excluir(int id);
+        Task Excluir(string id);
+
+
+    }
+    
+}
---
+++ Condominio.DAL/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Condominio.BLL.Models;
+using Condominio.DAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condominio.DAL.Repositorio
+{
+    public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
+    {
+        public UsuarioRepositorio(Contexto contexto) : base(contexto)
+        {
+        }
+    }
+}
